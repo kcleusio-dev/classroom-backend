b020f25cd206c643d9dd33f8cdcf0c6b17867754
kcleusio-dev <kcleusio.dev@gmail.com>
2026-03-25 15:26:44 +0100

    implementação do getAll Subjects

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import subjectsRouter from "./routes/subjects";
+import cors from 'cors';
+
+
+const app = express();
+const PORT = 8000;
+
+app.use(cors({
+    origin: process.env.FRONTEND_ORIGIN,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter);
+
+app.get('/', (req, res) => {
+    res.send('Wellcome to the Classroom API');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
